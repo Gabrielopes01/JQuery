f44473c40efcf153642a74d31df1b24a1a2a1d4f
Gabriel Lopes <gabrielperinotto@hotmail.com>
2021-02-05 08:41:26 -0300

    Eventos de Teclado (7/13)

--- js/scriptA7.js
+++ js/scriptA7.js
@@ -1,5 +1,47 @@
 $(function() {
 
+    $('.place').each(function() {
+        var place = $(this).attr('title');
+        var input = $(this);
+
+        console.log("Olllaa");
+
+        input
+            .val(place)
+            .css("color", "#ccc")
+            .focusin(function() {
+                input.css("color", "#000");
+                if(input.val() == place){
+                    input.val('');
+                }
+            })
+            .focusout(function() {
+                if(input.val() == ''){
+                    input
+                        .val(place)
+                        .css("color", "#ccc");
+                }
+            });
+    });
+
+
+    var ex = $('.ex1');
+    /*
+    //Sempre que uma tecla principal for digitada, mas com delay
+    $('.key').keypress(function() {
+        ex.text($(this).val());
+    });
+    */
+
+    //Sempre que uma tecla qualquer for pressionada, mas com delay
+    $('.key').keydown(function() {
+        ex.text($(this).val());
+    });
+
+    //Sempre que uma tecla qualquer for pressionada, em tempo real
+    $('.key').keyup(function() {
+        ex.text($(this).val());
+    });
 
     
 });
